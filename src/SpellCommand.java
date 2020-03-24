5bcf98e547502af8edf4c141ff391e576dda768e
Adam Pallante <adampallante@gmail.com>
2020-03-24 11:28:55 -0400

    Javadoc SpellCommmand.java

--- src/SpellCommand.java
+++ src/SpellCommand.java
@@ -1,11 +1,23 @@
-
+/**
+* Author: Team ME
+* This constucts the spell command which allows users to spell check the doc
+*
+*/
 public class SpellCommand {
 	private Document doc;
 
+	/**
+	* This constructs the spell command
+	* @param doc
+	*/
 	public SpellCommand(Document doc) {
 		this.doc = doc;
 	}
 
+  /**
+	* This will return to the user that the spell check is being performed on the doc
+	* @returns that the spell check on the doc has been completed
+	*/
 	public void execute() {
 		doc.spell();
 	}
