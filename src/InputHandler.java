1227a1e84abe08a7e33866ea08f203ec8b432341
Jake Powers <jpowers1324@gmail.com>
2020-03-23 20:53:20 -0400

    Updating SaveCommand and InputHandler

--- src/InputHandler.java
+++ src/InputHandler.java
@@ -4,7 +4,11 @@ public class InputHandler {
 	private HashMap<String, Command> commands;
 	
 	public InputHandler(Document document) {
-		
+		commands = new HashMap<>();
+		commands.put("load", LoadCommand);
+		commands.put("save", SaveCommand);
+		commands.put("spell", SpellCommands);
+		commands.put("print", PrintCommand);
 	}
 	
 	public void inputEntered(String data) {
--- src/SaveCommand.java
+++ src/SaveCommand.java
@@ -1,11 +1,12 @@
 
-public class SaveCommand implements Command{
+public class SaveCommand implements Command {
 	private Document doc;
-	SaveCommand(Document doc) {
+	
+	public SaveCommand(Document doc) {
 		this.doc = doc;
 	}
 	
 	public void execute() {
-		System.out.println("document " + doc + " is being saved...");
+		doc.save();
 	}
 }
