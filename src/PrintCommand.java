f268c56a7d05560a39608d015f70f10b0e8a7d71
Adam Pallante <adampallante@gmail.com>
2020-03-24 11:07:41 -0400

    Javadoc PrintCommand class

--- src/PrintCommand.java
+++ src/PrintCommand.java
@@ -1,11 +1,25 @@
 
 public class PrintCommand {
+	/**
+	* Author: Team ME
+	* Purpose of this class is to print the command that is being called
+	*/
 	private Document doc;
 
+	/**
+	* This method is meant to assign the variable doc to itself
+	* @param
+	*
+	*/
 	public PrintCommand(Document doc) {
 		this.doc = doc;
 	}
 
+  /**
+	* This method is executing the print command, which is telling the program to print the doc
+	* @param 
+	* @return	the printed doc
+	*/
 	public void execute() {
 		doc.print();
 	}
