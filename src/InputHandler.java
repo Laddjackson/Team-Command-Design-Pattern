feac2115ec8177833eeb10609fc26dc26128f135
Adam Pallante <adampallante@gmail.com>
2020-03-24 11:44:19 -0400

    Javadoc InputHandler.java #2

--- src/InputHandler.java
+++ src/InputHandler.java
@@ -8,6 +8,10 @@ public class InputHandler {
 	// Utilizing a HashMap to use the commands
 	private HashMap<String, Command> commands;
 
+	/**
+	* The purpose of the InputHandler is to map which command goes with what inputed
+	* @param document
+	*/
 	public InputHandler(Document document) {
 		commands = new HashMap<>();
 		commands.put("load", LoadCommand);
@@ -18,7 +22,7 @@ public class InputHandler {
 
 	/**
 	* This method is supposed to return the specific command that the user inputed
-	* @return data 
+	* @return data
 	*/
 	public void inputEntered(String data) {
 
