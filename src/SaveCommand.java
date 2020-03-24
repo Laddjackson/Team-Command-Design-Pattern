6c72fc751047457589e675a190e39b84cdf0ae0b
Adam Pallante <adampallante@gmail.com>
2020-03-24 11:25:21 -0400

    Javadoc SaveCommand.java

--- src/SaveCommand.java
+++ src/SaveCommand.java
@@ -1,11 +1,23 @@
-
+/**
+* Author: Team ME
+* The purpose of this class is to have the user be able to save the doc
+*
+*/
 public class SaveCommand implements Command {
 	private Document doc;
-	
+
+	/**
+	* This constucts the save command for the doc
+	* @param doc
+	*/
 	public SaveCommand(Document doc) {
 		this.doc = doc;
 	}
-	
+
+	/**
+	* This returns to the user that the document has or is being saved
+	* @returns the doc has been saved
+	*/
 	public void execute() {
 		doc.save();
 	}
