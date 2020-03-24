ba8754a648e018831d1c81ef9fd0d2fa15cc1aef
Adam Pallante <adampallante@gmail.com>
2020-03-24 11:22:29 -0400

    Javadoc LoadCommand.java

--- src/LoadCommand.java
+++ src/LoadCommand.java
@@ -1,11 +1,23 @@
-
+/**
+* Author: Team ME
+* This class creates the laod command so the user can load the doc
+*
+*/
 public class LoadCommand implements Command{
 	private Document doc;
-	
+
+	/**
+	* This constructs the load command
+	* @param doc
+	*/
 	public LoadCommand(Document doc) {
 		this.doc = doc;
 	}
-	
+
+	/**
+	* This returns that the doc is loading when called
+	* @returns doc.load
+	*/
 	public void execute() {
 		doc.load();
 	}
