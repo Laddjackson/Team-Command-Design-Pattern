beb215733a647bf3d7ff871cce6245eba616720d
Adam Pallante <adampallante@gmail.com>
2020-03-24 11:41:51 -0400

    Javadoc InputHandler.java

--- src/InputHandler.java
+++ src/InputHandler.java
@@ -1,8 +1,13 @@
 import java.util.HashMap;
-
+/**
+* Author: Team ME
+* The purpose of this class is supposed to handle all of the user input
+*
+*/
 public class InputHandler {
+	// Utilizing a HashMap to use the commands
 	private HashMap<String, Command> commands;
-	
+
 	public InputHandler(Document document) {
 		commands = new HashMap<>();
 		commands.put("load", LoadCommand);
@@ -10,9 +15,13 @@ public class InputHandler {
 		commands.put("spell", SpellCommands);
 		commands.put("print", PrintCommand);
 	}
-	
+
+	/**
+	* This method is supposed to return the specific command that the user inputed
+	* @return data 
+	*/
 	public void inputEntered(String data) {
-		
+
 	}
-	
+
 }
